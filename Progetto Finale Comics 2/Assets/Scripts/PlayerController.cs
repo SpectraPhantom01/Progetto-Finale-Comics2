bdbf33c069285b5780a752d9444d85605700bba2
SpectraPhantom01 <simo.bagliesi@gmail.com>
2022-05-12 21:32:49 +0200

    Add Damping on Movement

--- Progetto Finale Comics 2/Assets/Scripts/PlayerController.cs
+++ Progetto Finale Comics 2/Assets/Scripts/PlayerController.cs
@@ -6,13 +6,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Player Movement Settings")]
-    [SerializeField] float movementSpeed = 10f;
-    [SerializeField] float jumpForce = 20f;
+    [SerializeField] float movementSpeed = 10;
+    [SerializeField] float jumpForce = 20;
     [SerializeField] float jumpRememberTime = 0.2f;
     [Range(0f, 1f)]
     [SerializeField] float cutJumpValue = 0.5f;
-    //[Range(0f, 1f)]
-    //[SerializeField] float dampingValue = 0.5f;
+    //[SerializeField] float weightOnFalling = 10f;
+    [Range(0f, 1f)]
+    [SerializeField] float dampingValue = 0.5f;
     float jumpRememberTimer;
 
     [Header("Player Grounded Settings")]
@@ -21,22 +22,33 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float groundedRememberTime = 0.1f;
     [SerializeField] LayerMask groundLayer;
     float groundedRememberTimer;
-    public bool isGrounded;
+
 
     PlayerInput playerInput;
     Vector2 direction;
     Rigidbody2D rb;
+
+    bool isGrounded;
+
+    //Da togliere?
+    //bool isFalling;
+    //float acceleration = 10;
+    //float deceleration = 10;
+    //float currentSpeed = 0;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void FixedUpdate()
+    private void Update()
     {
         groundedRememberTimer -= Time.deltaTime;
         jumpRememberTimer -= Time.deltaTime;
+    }
 
+    void FixedUpdate()
+    {
         CheckGround();
         Movement();
     }
@@ -45,8 +57,17 @@ public class PlayerController : MonoBehaviour
     {
         direction = playerInput.Player.Movement.ReadValue<Vector2>();
         direction.y = 0;
-        //transform.Translate(direction * movementSpeed * Time.fixedDeltaTime);
-        rb.velocity = new Vector2(direction.x * movementSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(GetHorizontalVelocity(), rb.velocity.y);
+
+        //rb.velocity = new Vector2(direction.x * movementSpeed , rb.velocity.y);  
+    }
+
+    private float GetHorizontalVelocity()
+    {
+        float horizontal = rb.velocity.x;
+        horizontal += direction.x;
+        horizontal *= Mathf.Pow(1f - dampingValue, Time.deltaTime * 10f);
+        return horizontal;
     }
 
     public void Jump(UnityEngine.InputSystem.InputAction.CallbackContext obj)
@@ -89,6 +110,7 @@ public class PlayerController : MonoBehaviour
         playerInput.Player.Jump.performed += Jump;
         playerInput.Player.Jump.canceled += AbortJump;
     }
+
 
     private void OnDisable()
     {
