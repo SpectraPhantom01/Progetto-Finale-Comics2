f55604e211be0a060b75b99158dc2e4db900ff78
SpectraPhantom01 <simo.bagliesi@gmail.com>
2022-07-11 18:01:22 +0200

    Fix

--- Progetto Finale Comics 2/Assets/RecensioneDisplay.cs
+++ Progetto Finale Comics 2/Assets/RecensioneDisplay.cs
@@ -15,6 +15,7 @@ public class RecensioneDisplay : MonoBehaviour
 
     //public Image sfondo;
     public Image valutazione;
+
     // Start is called before the first frame update
     void Start()
     {
--- Progetto Finale Comics 2/Assets/Scripts/Test/Platform.cs
+++ Progetto Finale Comics 2/Assets/Scripts/Test/Platform.cs
@@ -20,6 +20,7 @@ public class Platform : MonoBehaviour
     private void Awake()
     {
         ResetPlatform();
+        GameManager.instance.onRespawn += ResetPlatform;
     }
 
     private void FixedUpdate()
@@ -70,13 +71,13 @@ public class Platform : MonoBehaviour
         }
         else
         {
-            ResetPlatform();
-            active = false;
+            ResetPlatform();       
         }
     }
 
     private void ResetPlatform()
     {
+        active = false;
         index = startingIndex;
         i = 1;
         transform.position = points[index].position;
