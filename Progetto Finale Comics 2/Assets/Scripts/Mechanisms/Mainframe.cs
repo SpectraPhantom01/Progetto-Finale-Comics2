d4549621a5e562e10457d612dac22bfd6dcfae15
SpectraPhantom01 <simo.bagliesi@gmail.com>
2022-07-17 10:29:46 +0200

    Mainframe

---
+++ Progetto Finale Comics 2/Assets/Scripts/Mechanisms/Mainframe.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class Mainframe : MonoBehaviour
+{
+    PlayableDirector director;
+
+    private void Start()
+    {
+        director = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        PlayerController player = collision.GetComponent<PlayerController>();
+        if (player.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            director.Play();
+        }
+    }
+
+}
